48ec06be0ee549a24f0fdf420c476eb65a310b95
AbbasNkundizana <your.abbasnkundizana@gmail.com>
2024-06-27 18:07:25 +0100

    first lines

---
+++ day1/printing.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+#print("She said: "Hello" and then left.")
+print('She said: "Hello" and then left.')
+print("She said: \"Hello\" and then left.")
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
